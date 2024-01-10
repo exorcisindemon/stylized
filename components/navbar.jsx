7e5df2c972607cf4ea5af0467db0a591897827b8
sievl <poxcia@outlook.com>
2024-01-10 14:15:52 +0700

    update

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const { default: Search } = require("../shared/icons/search-line.svg");
 const {
@@ -15,53 +17,100 @@ const {
 } = require("../shared/icons/create-new-account-line.svg");
 
 const Navbar = () => {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const navbarHandler = () => {
+    setExpandNavbar(!expandNavbar);
+  };
+
+  useEffect(() => {
+    const breakpointsHandler = window.innerWidth <= 768;
+    setExpandNavbar(!breakpointsHandler);
+  }, []);
+
   return (
-    <div className="bg-white text-white text-sm p-4 flex flex-col lg:flex-row gap-4 lg:gap-2 items-start lg:items-center justify-between w-full">
-      <div className="flex flex-col lg:flex-row gap-4 lg:gap-2 items-start lg:items-center w-full lg:w-auto">
-        <h1 className="font-semibold bg-transparent text-black">STYLIZED</h1>
+    <div
+      className={`${
+        expandNavbar ? "h-screen lg:h-auto" : "h-auto"
+      } bg-white text-white text-sm p-4 flex flex-col lg:flex-row gap-8 lg:gap-2 items-start lg:items-center justify-between w-full`}
+    >
+      <div className="flex flex-col lg:flex-row gap-8 lg:gap-2 items-start lg:items-center w-full lg:w-auto">
+        <div className="flex flex-row gap-4 lg:gap-2 items-center justify-between lg:justify-normal w-full lg:w-auto">
+          <div className="flex flex-col lg:flex-row gap-2">
+            <h1 className="font-semibold bg-transparent text-black">
+              STYLIZED
+            </h1>
+            <h1 className="font-medium bg-transparent text-black/80">
+              Find your style
+            </h1>
+          </div>
+          <button
+            className="flex lg:hidden bg-black/10 p-2 rounded-lg"
+            onClick={navbarHandler}
+          >
+            <Image
+              className={`${
+                expandNavbar ? "rotate-180" : "rotate-0"
+              } transition-all`}
+              src={ArrowDown}
+              width="16"
+              height="16"
+            />
+          </button>
+        </div>
         <label className="hidden lg:flex font-semibold bg-transparent text-black">
           |
         </label>
-        <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={CreateNewAccount} width="16" height="16" />
-          Fashion
-        </button>
-        <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={CreateNewAccount} width="16" height="16" />
-          Promo
-        </button>
-        <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={DownloadApp} width="16" height="16" />
-          Download App
-        </button>
+        {expandNavbar && (
+          <>
+            <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
+              <Image src={CreateNewAccount} width="16" height="16" />
+              Fashion
+            </button>
+            <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
+              <Image src={CreateNewAccount} width="16" height="16" />
+              Promo
+            </button>
+            <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
+              <Image src={DownloadApp} width="16" height="16" />
+              Download App
+            </button>
+          </>
+        )}
       </div>
-      <div className="flex flex-row gap-2 items-center bg-black/10 outline-none w-[20%] font-semibold text-black rounded-lg px-2 py-2">
-        <input
-          placeholder={["2000s outfit", "Long Sleeve Shirt"]}
-          className="bg-transparent w-full outline-none"
-          type="text"
-        />
-        <Image src={Search} width="16" height="16" />
-      </div>
-      <div className="flex flex-col lg:flex-row gap-4 lg:gap-2 items-start lg:items-center w-full lg:w-auto">
-        <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={CustomerService} width="16" height="16" />
-          Help Center
-        </button>
-        <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={Translate} width="16" height="16" />
-          English
-          <Image src={ArrowDown} width="16" height="16" />
-        </button>
-        <button className="transition-all lg:hover:bg-black/90 flex flex-row gap-2 items-center font-semibold bg-black text-white rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={Login} width="16" height="16" />
-          Log in
-        </button>
-        <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
-          <Image src={CreateNewAccount} width="16" height="16" />
-          Create new Account
-        </button>
-      </div>
+      {expandNavbar && (
+        <>
+          <div className="flex flex-row gap-2 items-center bg-black/10 outline-none w-full lg:w-[20%] font-semibold text-black rounded-lg px-2 py-2">
+            <input
+              placeholder={["2000s outfit", "Long Sleeve Shirt"]}
+              className="bg-transparent w-full outline-none"
+              type="text"
+            />
+            <Image src={Search} width="16" height="16" />
+          </div>
+          <div className="flex flex-col lg:flex-row gap-8 lg:gap-2 items-start lg:items-center w-full lg:w-auto">
+            <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
+              <Image src={CustomerService} width="16" height="16" />
+              Help Center
+            </button>
+            <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-none lg:rounded-lg p-0 lg:px-2 lg:py-2">
+              <Image src={Translate} width="16" height="16" />
+              English
+              <Image src={ArrowDown} width="16" height="16" />
+            </button>
+            <div className="flex flex-row gap-4 lg:gap-2 items-start lg:items-center justify-between lg:justify-normal w-full lg:w-auto">
+              <button className="transition-all lg:hover:bg-black/90 flex flex-row gap-2 items-center font-semibold bg-black text-white rounded-lg p-2 lg:px-2 lg:py-2">
+                <Image src={Login} width="16" height="16" />
+                Log in
+              </button>
+              <button className="transition-all lg:hover:bg-black/10 flex flex-row gap-2 items-center font-semibold bg-transparent text-black rounded-lg p-2 lg:px-2 lg:py-2">
+                <Image src={CreateNewAccount} width="16" height="16" />
+                Create new Account
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
